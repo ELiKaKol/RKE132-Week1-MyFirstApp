561852b5d84fb653cc86db771ae459906b6505c1
EliKaKol <elisak.kolga@gmail.com>
2024-09-21 13:06:10 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisetada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string-sõne
+string userName = Console.ReadLine(); //Input
+
+
+Console.WriteLine("Hello" + ", " + userName +"!"); //Output
+
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !"); //Output
+
+
